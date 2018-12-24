eb98b8b117836d10453fe6f713f495ac22034d28
Tom Hayosh <thayosh@gmail.com>
2018-12-24 15:06:14 -0600

    Split out unit tests into two files

--- functional_tests/test_list_item_validation.py
+++ functional_tests/test_list_item_validation.py
@@ -1,5 +1,6 @@
 from .base import FunctionalTest
-from unittest import skip
+from selenium.webdriver.common.keys import Keys
+# from unittest import skip
 
 MAX_WAIT = 10
 
@@ -7,7 +8,37 @@ MAX_WAIT = 10
 class ItemValidationTest(FunctionalTest):
 
     def test_cannot_add_empty_list_items(self):
-        self.fail("write me!")
+        # Edith goes to the home page and accidentally tries to submit
+        # an empty list item. She hits Enter on the empty input box
+        self.browser.get(self.live_server_url)
+        self.browser.find_element_by_id('id_new_item').send_keys(Keys.ENTER)
+
+        # The home page refreshes, and there is an error message saying
+        # that list items cannot be blank
+        self.wait_for(lambda: self.assertEqual(
+            self.browser.find_element_by_css_selector('.has-error').text,
+            "You can't have an empty list item"
+        ))
+
+        # She tries again with some text for the item, which now works
+        self.browser.find_element_by_id('id_new_item').send_keys('Buy milk')
+        self.browser.find_element_by_id('id_new_item').send_keys(Keys.ENTER)
+        self.wait_for_row_in_list_table('1: Buy milk')
+
+        # Perversely, she now decides to submit a second blank list item
+        self.browser_find_element_by_id('id_new_item').send_keys(Keys.ENTER)
+
+        # She receives a similar warning on the list page
+        self.wait_for(lambda: self.assertEqual(
+            self.browser.find_element_by_css_selector('.has_error').text,
+            "You can't have an empty list item"
+        ))
+
+        # And she can correct it by filling some text in
+        self.browser.find_element_by_id('id_new_item').send_keys('Make tea')
+        self.browser.find_element_by_id('id_new_item').send_keys(Keys.ENTER)
+        self.wait_for_row_in_list_table('1: Buy milk')
+        self.wait_for_row_in_list_table('1: Make tea')
 
 # Edith wonders whether the site will remember her list. Then she sees
 # that the site has generated a unique URL for her -- there is some
